0f9549ea3c27c0a5a906ebda01f6ca11099f3b8d
aleksesiy <lexa.svsk@gmail.com>
2024-12-26 16:08:48 +0700

    feat: more done

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,10 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//function getCommonCharacterCount(s1, s2) {
+//  s1 = s1.split('');
+//  let count = 0;
+//  for (let i = 0; i < s2.length; i++) {
+//    if (s1.includes(s2[i])) {
+//      count++;
+//    }
+//  }
+//  return count;
+//}
+
+
+function getCommonCharacterCount(s1, s2) {
+  s1 = s1.split('');
+  let count = 0;
+
+  for (const char of s2) {
+    const index = s1.indexOf(char);
+    if (index !== -1) {
+      count++;
+      s1.splice(index, 1);
+    }
+  }
+
+  return count;
 }
+
 
 module.exports = {
   getCommonCharacterCount
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,21 +1,22 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given an array with heights, sort them except if the value is -1.
- *
  * @param {Array} arr
  * @return {Array}
- *
  * @example
- * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
- *
- * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
+ * arr = [-1, 150, 190, 170, -1, -1, 160, 180, 190]
+ * The result should be [-1, 150, 160, 170, -1, -1, 180, 190, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  // Extract all the non -1 values
+  const sortedValues = arr.filter(x => x !== -1).sort((a, b) => a - b);
+
+  // Replace non -1 values in the original array with the sorted values
+  let index = 0;
+  return arr.map(x => (x === -1 ? x : sortedValues[index++]));
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
+
